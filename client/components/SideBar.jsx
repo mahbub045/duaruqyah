b2b3a6cd9e348d8807c58c583652e17c4b6cdf05
mahbub045 <mahbubrmn45@gmail.com>
2024-01-31 23:33:44 +0600

    Side Bar complete

---
+++ client/components/SideBar.jsx
@@ -0,0 +1,105 @@
+import Image from "next/image";
+
+const SideBar = () => {
+  return (
+    <>
+      <div className="bg-red-200 flex flex-col justify-center items-center">
+        {/* logo */}
+        <div className="p-16">
+          <a href="">
+            <Image src="/images/logo.png" alt="Logo" width={93} height={93} />
+          </a>
+        </div>
+        <div>
+          {/* Menu Items */}
+          <ul className="">
+            <li className="my-5 bg-[#E8F0F5] p-2 rounded-full transform hover:scale-105">
+              <a href="">
+                <Image
+                  src="/images/home 1.svg"
+                  alt="Logo"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </li>
+            <li className="my-5 bg-[#E8F0F5] p-2 rounded-full transform hover:scale-105">
+              <a href="">
+                <Image
+                  src="/images/54-menu-2.svg"
+                  alt="img"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </li>
+            <li className="my-5 bg-[#E8F0F5] p-2 rounded-full transform hover:scale-105">
+              <a href="">
+                <Image
+                  src="/images/memorize 1.svg"
+                  alt="img"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </li>
+            <li className="my-5 bg-[#E8F0F5] p-2 rounded-full transform hover:scale-105">
+              <a href="">
+                <Image
+                  src="/images/3-keep-minus.svg"
+                  alt="img"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </li>
+            <li className="my-5 bg-[#E8F0F5] p-2 rounded-full transform hover:scale-105">
+              <a href="">
+                <Image
+                  src="/images/ruqyah 1.svg"
+                  alt="img"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </li>
+            <li className="my-5 bg-[#E8F0F5] p-2 rounded-full transform hover:scale-105">
+              <a href="">
+                <Image
+                  src="/images/dua-info 1.svg"
+                  alt="img"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </li>
+            <li className="my-5 bg-[#E8F0F5] p-2 rounded-full transform hover:scale-105">
+              <a href="">
+                <Image
+                  src="/images/books 1.svg"
+                  alt="img"
+                  width={20}
+                  height={20}
+                />
+              </a>
+            </li>
+          </ul>
+        </div>
+        <div className="p-16">
+          {/* support icon */}
+          <a href="">
+            <Image
+              className="rounded-md bg-green-500 shadow-md"
+              src="/images/donate-heart.png"
+              alt="donate"
+              width={40}
+              height={40}
+            />
+          </a>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default SideBar;
